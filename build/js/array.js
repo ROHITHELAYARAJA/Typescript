eb467c5a096f04ebaec50656b683f62257f62c9c
ROHITHELAYARAJA <e.rohith3130@gmail.com>
2026-08-28 11:55:14 +0530

    feat: implement array iteration and manipulation logic in new array module

---
+++ build/js/array.js
@@ -0,0 +1,10 @@
+let strArr = ["rohith", "kaviya", "divya"];
+for (let i = 0; i < strArr.length; i++) {
+    console.log(strArr[i]);
+}
+let numArr = [1, 2, "hii", 4, 5];
+for (let i = 0; i < numArr.length; i++) {
+    console.log(numArr[i]);
+}
+strArr[0] = '19';
+export {};
---
+++ src/array.ts
@@ -0,0 +1,12 @@
+let strArr: [string, number] = ["rohith", 1];
+for (let i = 0; i < strArr.length; i++) {
+    console.log(strArr[i]);
+}
+
+let numArr: any[] = [1, 2, "hii", 4, 5];
+for (let i = 0; i < numArr.length; i++) {
+    console.log(numArr[i]);
+}
+
+strArr[0]='19';
+
